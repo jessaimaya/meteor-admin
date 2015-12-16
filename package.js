398e84a220db7edacb376cd3ddb0f7647b9e6d73
jessai <jessai@kamikazelab.com>
2015-12-16 11:43:28 -0600

    Admin fix

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "yogiben:admin",
+  name: "physicsteacher:admin",
   summary: "A complete admin dashboard solution",
   version: "1.2.3",
   git: "https://github.com/yogiben/meteor-admin"
